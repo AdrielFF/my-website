505d01b87a49e53e3a2c195a3b4d1f8071ac6d6d
AdrielFF <adrielfernandes321@gmail.com>
2019-04-26 19:50:15 -0300

    Created responsive mobile-menu

---
+++ assets/javascripts/mobile-menu.js
@@ -0,0 +1,7 @@
+
+let menuTrigger = document.querySelector(".nav")
+let menu = document.querySelector(".menu-nav")
+
+menuTrigger.addEventListener("click", () => {
+  menu.classList.toggle("active")
+})
