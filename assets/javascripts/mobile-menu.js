8ee56881d31907cc879db28e7b0e9f67171bb87c
AdrielFF <adrielfernandes321@gmail.com>
2019-04-26 23:18:24 -0300

    Created script to hide menu when scroll down with it opened

--- assets/javascripts/mobile-menu.js
+++ assets/javascripts/mobile-menu.js
@@ -4,3 +4,9 @@ let menu = document.querySelector(".menu-nav")
 menuTrigger.addEventListener("click", () => {
   menu.classList.toggle("active")
 })
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY >= 100 && menu.classList.contains("active")){
+    menu.classList.toggle("active")
+  }
+})
